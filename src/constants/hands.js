353d50f7b4c7ccfd2c035b43af07ec8f96c96b79
seosigoto <seo.sigoto@gmail.com>
2023-11-28 03:08:50 +1100

    feat: fix orders of constants

--- src/constants/hands.js
+++ src/constants/hands.js
@@ -1,5 +1,5 @@
 export const Hands = [
   { name: "ROCK", icon: "✊", value: 0 },
-  { name: "PAPER", icon: "✋", value: 1 },
-  { name: "SCISSORS", icon: "✌️", value: 2 },
+  { name: "SCISSORS", icon: "✌️", value: 1 },
+  { name: "PAPER", icon: "✋", value: 2 },
 ];
